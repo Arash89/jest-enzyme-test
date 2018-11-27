14068b864221987607ca64cf8d68f84d920fdb79
Arash Soudishoar <b_arash_s@yahoo.com>
2018-11-26 16:23:02 -0800

    jest.fn some tests

--- src/arash.js
+++ src/arash.js
@@ -1,1 +1,7 @@
 export const salam = () => "Salam";
+
+export const bodyFunction = (usedM) => {
+    console.log("Line 1:", usedM());
+    console.log("Line 2:", usedM());
+    usedM();
+};
--- src/arash.test.js
+++ src/arash.test.js
@@ -1,6 +1,17 @@
-import {salam} from "./arash";
+import {salam, bodyFunction} from "./arash";
+console.log("Arash inam yeh testeh 22");
 it("This is for the the test", () => {
+
     expect(salam()).toBe("Salam");
-}); 
+});
+
+it("Body is going to be tested", () => {
+    const myMock = jest.fn();
+    const slamMock = jest.fn(salam);
+    bodyFunction(slamMock);
+    expect(slamMock.mock.calls.length).toBe(3);
+    expect(slamMock).toHaveBeenCalledTimes(3);
+    expect(slamMock.mock.calls[2][0]).toBeUndefined();
+});
 
 
