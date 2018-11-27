d7a0e310f65f2bf5a3f5ab930ed545401eb827e5
Arash Soudishoar <b_arash_s@yahoo.com>
2018-11-26 17:54:47 -0800

    jest.fn for Mocking functions. Using .mock properties.

--- src/arash.test.js
+++ src/arash.test.js
@@ -7,11 +7,22 @@ it("This is for the the test", () => {
 
 it("Body is going to be tested", () => {
     const myMock = jest.fn();
-    const slamMock = jest.fn(salam);
-    bodyFunction(slamMock);
-    expect(slamMock.mock.calls.length).toBe(3);
-    expect(slamMock).toHaveBeenCalledTimes(3);
-    expect(slamMock.mock.calls[2][0]).toBeUndefined();
+    const salamMock = jest.fn(salam);
+    salamMock.mockReturnValue("Chakeram");
+
+    for (let i = 0; i < 2; i++) {
+        if (i === 1) {
+            salamMock.mockReturnValueOnce(100);
+        } else {
+            salamMock.mockReturnValueOnce(salam());
+        }
+    }
+
+    bodyFunction(salamMock);
+    expect(salamMock.mock.calls.length).toBe(3);
+    expect(salamMock).toHaveBeenCalledTimes(3);
+    expect(salamMock.mock.calls[2][0]).toBeUndefined();
+    expect(salamMock.mock.results[0].value).toBe("Salam");
 });
 
 
