840c457ba7ba9df0c2271afa9a60369a4b02620a
Arash Soudishoar <b_arash_s@yahoo.com>
2018-12-02 17:13:38 -0800

    point 1: If f1 calls f2 inside its body and f2 is defined in another file and imported in the Module file there is no need to import f2

--- src/arash.js
+++ src/arash.js
@@ -1,9 +1,13 @@
+// point 2
+import { areYouWell } from "./arashUtil";
 export const salam = () => "Salam";
 
+// point 1
 export const whatsUp = () => "What is up man?";
 
 export const bodyFunction = (usedM) => {
     console.log("Line 1:", usedM());
     console.log("Line 2:", whatsUp());
+    console.log("Are you well?", areYouWell(true));
     usedM();
 };
---
+++ src/arashUtil.js
@@ -0,0 +1,3 @@
+export const areYouWell = (mode) => {
+    return mode ? "Well" : "Not so well";
+};
